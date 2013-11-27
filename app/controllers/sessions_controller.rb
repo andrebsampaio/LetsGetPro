03a34427fd4cff9e192ae4d9accfc4d33f8d24f0
absampaio <andrebsampaio@gmail.com>
2013-11-27 16:19:11 +0000

    Everything v0.2 + Working Login v.1.2

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+  before_filter :authenticate_user, :only => [:home, :profile, :setting]
+  before_filter :save_login_state, :only => [:login, :login_attempt]
+
+  layout 'new'
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      redirect_to root_path
+    else
+      flash[:error] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
+  def save_login_state
+    if session[:user_id]
+      redirect_to(:controller => 'sessions', :action => 'home')
+      return false
+    else
+      return true
+    end
+  end
+end
